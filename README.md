f86b07780a90aedfd393524d1ee79b05503e3fd0
ItaloAlulas <183663705+ItaloAlulas@users.noreply.github.com>
2025-07-04 18:30:07 -0300

    Code finished, a lot of features were added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rockPaperScissors
+
+This is just my resolution of an exercise from TOP.
--- script.js
+++ script.js
@@ -6,15 +6,92 @@ function getComputerChoice() {
     switch (choice) {
         case 1:
             return "Rock";
-        case 2: 
+        case 2:
             return "Paper";
         case 3:
             return "Scissor";
     }
 }
 
-// User input to choose between the options.
+// User input to choose between the options in the game.
 function getHumanChoice() {
 
-    return prompt('Write your choice (Rock / Paper / Scissor): ');
+    // Get the user choice and transform the string, having only lowercase letters and no space in the margins.
+    let choice = prompt('Write your choice (Rock / Paper / Scissor): ').trim().toLowerCase();
+
+    switch (choice) {
+        case 'rock':
+            return 'Rock';
+        case 'paper':
+            return 'Paper';
+        case 'scissor':
+            return 'Scissor';
+    }
 }
+
+// Keeping track of the computer and player score
+let computerScore = 0;
+let humanScore = 0;
+
+// Play a round in the game
+function playRound(computerChoice, humanChoice) {
+    computerChoice = getComputerChoice();
+    humanChoice = getHumanChoice();
+
+    // When it's a draw
+    if (humanChoice === 'Rock'    && computerChoice === 'Rock' ||
+        humanChoice === 'Paper'   && computerChoice === 'Paper' ||
+        humanChoice === 'Scissor' && computerChoice === 'Scissor') {
+
+        console.log(`
+It's a DRAW!
+PC:  ${computerScore}
+YOU: ${humanScore}`);
+    }
+    // When user wins
+    else if (humanChoice === 'Rock'    && computerChoice === 'Scissor' ||
+             humanChoice === 'Paper'   && computerChoice === 'Rock'    ||
+             humanChoice === 'Scissor' && computerChoice === 'Paper') {
+
+        humanScore++;
+        console.log(`
+You WIN! ${humanChoice} beats ${computerChoice}!
+PC: ${computerScore}
+YOU: ${humanScore}`);
+    }
+    // When user loses
+    else {
+        computerScore++;
+        console.log(`
+You LOSE! ${humanChoice} loses to ${computerChoice}!
+PC: ${computerScore}
+YOU: ${humanScore}`);
+    }
+}
+
+// Play 5 rounds at once and defines a winner
+function playGame() {
+    for (i = 0; i < 5; i++) {
+       playRound();
+    }
+    if (humanScore > computerScore) {
+        console.log(`
+!!! YOU ARE THE WINNER !!!
+PC:  ${computerScore}
+YOU: ${humanScore}`);
+
+    } else if (computerScore > humanScore) {
+        console.log(`
+!!! COMPUTER IS THE WINNER !!!
+PC:  ${computerScore}
+YOU: ${humanScore}`);
+
+    } else {
+        console.log(`
+!!! DEFINITIVE DRAW !!!
+PC:  ${computerScore}
+YOU: ${humanScore}`);
+    }
+}
+
+playGame();
